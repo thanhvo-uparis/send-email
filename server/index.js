3ce7ee1c7fcd277d5f59d50d4f25c3805d5ff82a
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-01-30 23:39:28 +0100

    connect to mysql

--- server/index.js
+++ server/index.js
@@ -2,10 +2,29 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mysql = require("mysql2");
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "send-email"
+});
+
+app.get("/api/get", (request, response) => {
+    const sqlGet = "SELECT * FROM users "; 
+    db.query(sqlGet, (error, result) => {
+        response.send(result)
+    })
+});
+
+app.get("/", (request, response) => {
+    response.send("Server is running!");
+});
 
 const PORT = 5000;
 
